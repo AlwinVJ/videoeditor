9121c9ada0a4f3e71d824d492c54e656675f24ac
AlwinVJ <alwinvj5@gmail.com>
2026-05-14 19:55:17 +0530

    Changed video_io.py file for streamlit execution

--- core/video_io.py
+++ core/video_io.py
@@ -29,11 +29,9 @@ def process_video(
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    # Conservative worker count
-    # cpu_count = os.cpu_count() or 4
-    # max_workers = max(1, min(cpu_count - 1, 4))
-    
-    max_workers = 1
+    Conservative worker count
+    cpu_count = os.cpu_count() or 4
+    max_workers = max(1, min(cpu_count - 1, 4))
 
     batch_size = 12
 
