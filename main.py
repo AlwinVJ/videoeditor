a1cd6add64e72326c863eede0d3c065e5f471a5a
AlwinVJ <alwinvj5@gmail.com>
2026-05-14 18:09:52 +0530

    Remove connection error issues after processing

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import asyncio
 from multiprocessing import freeze_support
 
 from app.main import run_app
 
 if __name__ == "__main__":
     freeze_support()
+    
+    asyncio.set_event_loop_policy(
+        asyncio.WindowsSelectorEventLoopPolicy()
+    )
+    
     run_app()
